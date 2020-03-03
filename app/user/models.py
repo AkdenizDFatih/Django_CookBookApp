cec90da82f5a8518de7ab14e8d4f9c7128a8a237
cdelacombaz <cdelacombaz@bluewin.ch>
2020-03-03 10:59:54 +0100

    Updated custom user model - removed redundancy (inherited fields)

--- app/user/models.py
+++ app/user/models.py
@@ -1,49 +1,15 @@
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
+    # Field used for authentication
     USERNAME_FIELD = 'email'
 
+    # Additional fields required when using createsuperuser (USERNAME_FIELD and passwords are always required)
     REQUIRED_FIELDS = ['username']
 
     email = models.EmailField(unique=True)
-
-    username = models.CharField(
-        max_length=200,
-        unique=True
-    )
-
-    first_name = models.CharField(
-        verbose_name='first name',
-        max_length=200,
-        blank=True,
-    )
-
-    last_name = models.CharField(
-        verbose_name='last name',
-        max_length=200,
-        blank=True,
-    )
-
-    is_staff = models.BooleanField(
-        verbose_name='staff status',
-        default=False,
-        help_text='Designates whether the user can log into this site.',
-    )
-
-    is_active = models.BooleanField(
-        verbose_name='active',
-        default=True,
-        help_text='Designates whether this user should be treated as active. '
-                  'Unselect this instead of deleting accounts.'
-    )
-
-    date_joined = models.DateTimeField(
-        verbose_name='date joined',
-        auto_now_add=True
-    )
 
     def __str__(self):
         return self.username
